eb436180e82b940ac772b5544fc019810b5ba75f
domica-s <domica@link.cuhk.edu.hk>
2021-07-16 14:49:42 +0700

    remove /markets/ slug

--- markets/urls.py
+++ markets/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('markets/', views.index, name='markets'),
-    path('markets/crypto/<slug:coin_slug>', views.crypto_details, name='crypto-details')
+    path('', views.index, name='markets'),
+    path('crypto/<slug:coin_slug>', views.crypto_details, name='crypto-details')
 ]
