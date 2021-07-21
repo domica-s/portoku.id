bd2e263c54a23d0f79f1e38d0cbbd7d4f3073fda
bryanwilsonkheng <bryanwilsonkheng@gmail.com>
2021-07-21 23:09:02 +0700

    Modify models

--- markets/models.py
+++ markets/models.py
@@ -2,15 +2,15 @@ from django.db import models
 
 # Create your models here.
 class Coin(models.Model):
-    name = models.CharField(max_length=200)
-    fullname = models.CharField(max_length=200)
+    name = models.CharField(max_length=200, unique=True)
+    fullname = models.CharField(max_length=200, default="COINxNAME")
     slug = models.SlugField(unique=True)
     price = models.FloatField()
     description = models.TextField()
 
 class Stock(models.Model):
-    name = models.CharField(max_length=200)
-    fullname = models.CharField(max_length=200)
+    name = models.CharField(max_length=200, unique=True)
+    fullname = models.CharField(max_length=200, default="STOCKxNAME")
     slug = models.SlugField(unique=True)
     price = models.FloatField()
     description = models.TextField()
