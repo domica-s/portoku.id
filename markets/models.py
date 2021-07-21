23859c27a81ead2785e6376d7f4592f1e947e86a
bryanwilsonkheng <bryanwilsonkheng@gmail.com>
2021-07-21 22:24:17 +0700

    Add fullname fields in crypto and stocks

--- markets/models.py
+++ markets/models.py
@@ -3,12 +3,14 @@ from django.db import models
 # Create your models here.
 class Coin(models.Model):
     name = models.CharField(max_length=200)
+    fullname = models.CharField(max_length=200)
     slug = models.SlugField(unique=True)
     price = models.FloatField()
     description = models.TextField()
 
 class Stock(models.Model):
     name = models.CharField(max_length=200)
+    fullname = models.CharField(max_length=200)
     slug = models.SlugField(unique=True)
     price = models.FloatField()
     description = models.TextField()
