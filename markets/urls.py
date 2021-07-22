5515df8e118232827887b6bcb9596a5451f88bab
bryanwilsonkheng <bryanwilsonkheng@gmail.com>
2021-07-22 17:38:44 +0700

    Modifying dropdown for base.html

--- markets/urls.py
+++ markets/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='markets'),
     path('crypto/<slug:coin_slug>', views.crypto_details, name='crypto-details'),
-    path('stocks/<slug:stock_slug>', views.stock_details, name='stock-details')
+    path('stock/<slug:stock_slug>', views.stock_details, name='stock-details')
 ]
