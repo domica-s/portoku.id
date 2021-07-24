d89d6e593a7e894ea508593113c9815b52537ac6
domica-s <domica@link.cuhk.edu.hk>
2021-07-24 11:48:46 +0700

    fix message not showing

--- portoku_id/settings.py
+++ portoku_id/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+from django.contrib.messages import constants as messages
+
 
 SETTINGS_PATH = os.path.dirname(os.path.dirname(__file__))
 
@@ -133,3 +135,11 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+MESSAGE_TAGS = {
+    messages.DEBUG: 'alert-info',
+    messages.INFO: 'alert-info',
+    messages.SUCCESS: 'alert-success',
+    messages.WARNING: 'alert-warning',
+    messages.ERROR: 'alert-danger',
+}
