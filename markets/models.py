e96fc5f2f354d797b13f740950a323bfd7b821c9
domica-s <domica@link.cuhk.edu.hk>
2021-07-26 18:26:57 +0700

    add data.json (data from yfinance for 783 tickers); update stock moedl

--- markets/models.py
+++ markets/models.py
@@ -11,6 +11,21 @@ class Coin(models.Model):
 class Stock(models.Model):
     name = models.CharField(max_length=200, unique=True)
     fullname = models.CharField(max_length=200, default="STOCKxNAME")
-    slug = models.SlugField(unique=True)
+    slug = models.SlugField(unique=True) # will be the same as name
+    sector = models.CharField(max_length=200)
+    industry = models.CharField(max_length=200)
+    numOfShares = models.IntegerField()
+    # below are dynamic datas
     price = models.FloatField()
-    description = models.TextField()
+    open = models.FloatField()
+    high = models.FloatField()
+    low = models.FloatField()
+    close = models.FloatField()
+    change24h = models.FloatField()
+    volume = models.FloatField()
+
+    def calculateMarketCap(self):
+        return Stock.objects.filter(name=self).price * Stock.objects.filter(name=self).numOfShares 
+
+    marketCap = property(calculateMarketCap)
+
