76ab0a2ac2cb28fc123bccee69e3753408226146
domica-s <domica@link.cuhk.edu.hk>
2021-07-29 14:59:00 +0700

    update Stock model, change stock-details.html

--- markets/models.py
+++ markets/models.py
@@ -8,10 +8,28 @@ class Coin(models.Model):
     price = models.FloatField()
     description = models.TextField()
 
+# class Stock(models.Model):
+#     name = models.CharField(max_length=200, unique=True)
+#     fullname = models.CharField(max_length=200, default="STOCKxNAME")
+#     slug = models.SlugField(unique=True)
+#     price = models.FloatField()
+#     description = models.TextField()
+
 class Stock(models.Model):
     name = models.CharField(max_length=200, unique=True)
     fullname = models.CharField(max_length=200, default="STOCKxNAME")
-    slug = models.SlugField(unique=True)
+    slug = models.SlugField(unique=True) # will be the same as name
+    sector = models.CharField(max_length=200)
+    industry = models.CharField(max_length=200)
+    numOfShares = models.IntegerField()
+    # below are dynamic datas
+    open = models.FloatField()
+    high = models.FloatField()
+    low = models.FloatField()
+    close = models.FloatField()
+    change24h = models.FloatField()
+    volume = models.FloatField()
+    marketCap = models.FloatField()
+    # update periodically, test this first
     price = models.FloatField()
-    description = models.TextField()
 
--- markets/views.py
+++ markets/views.py
@@ -44,15 +44,16 @@ def crypto_details(request, coin_slug):
 
 def stock_details(request, stock_slug):
     selected_stock = Stock.objects.get(slug=stock_slug)
-    ticker = selected_stock.name + ".JK"
+    # ticker = selected_stock.name + ".JK"
     return render(request, 'markets/stock-details.html',
         {
-            'name' : selected_stock.name,
-            'fullname' : getFullStockName(ticker),
-            'slug' : selected_stock.name,
-            'price' : getStockSpotPrice(ticker),
-            'data' : getStockGraph(ticker),
-            'description' : getStockSummary(ticker)
+            # 'name' : selected_stock.name,
+            # 'fullname' : getFullStockName(ticker),
+            # 'slug' : selected_stock.name,
+            # 'price' : getStockSpotPrice(ticker),
+            # 'data' : getStockGraph(ticker),
+            # 'description' : getStockSummary(ticker)
+            'stocks' : selected_stock,
         }
     )
 
